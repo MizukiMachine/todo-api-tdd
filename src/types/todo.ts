35ef03bb2f9e9935aa7972ffbddd80e00fc59954
Mizuki Tamura <negichanfumakun@gmail.com>
2025-01-15 16:39:03 +0900

    カスタムエラーの定義

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -17,3 +17,20 @@ export interface UpdateTodoDTO {
   title?: string;      // タイトルの更新（オプション）
   completed?: boolean; // 完了状態の更新（オプション）
 }
+
+// Todo未検出時のエラー
+export class TodoNotFoundError extends Error {
+  constructor(id: string) {
+      super(`Todo with id ${id} not found`);
+      this.name = 'TodoNotFoundError';
+  }
+}
+
+// バリデーションエラー
+export class InvalidTodoError extends Error {
+  constructor(message: string) {
+      super(message);
+      this.name = 'InvalidTodoError';
+  }
+}
+
