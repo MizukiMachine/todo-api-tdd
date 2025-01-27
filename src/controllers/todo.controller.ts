04f7f180185f2d729f4895e8216190c61eb0ea79
Mizuki Tamura <negichanfumakun@gmail.com>
2025-01-27 16:05:23 +0900

    コントローラの実装

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 import { TodoService } from '../services/todo.service';
 import { CreateTodoDTO } from '../types';
 
@@ -7,13 +8,26 @@ export class TodoController {
 
     async createTodo(req: Request, res: Response): Promise<void> {
         try {
+            // バリデーション結果のチェック
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                res.status(400).json({ 
+                    errors: errors.array().map(err => err.msg)
+                });
+                return;
+            }
+
             const todo = await this.service.createTodo(req.body as CreateTodoDTO);
             res.status(201).json(todo);
         } catch (error) {
             if (error instanceof Error) {
-                res.status(400).json({ error: error.message });
+                res.status(400).json({ 
+                    errors: [error.message]
+                });
             } else {
-                res.status(500).json({ error: 'Internal server error' });
+                res.status(500).json({ 
+                    errors: ['Internal server error']
+                });
             }
         }
     }
